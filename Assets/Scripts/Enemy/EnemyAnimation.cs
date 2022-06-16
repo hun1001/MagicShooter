d2f6c06a7f84e96adb5afe08d44b2668e37e4291
hun1001 <l01022899266@gmail.com>
2022-06-16 15:59:05 +0900

    진짜 많이 밉다

---
+++ Assets/Scripts/Enemy/EnemyAnimation.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    [Header("Animation clip")]
+    [SerializeField]
+    private AnimationClip _idleClip = null;
+    [SerializeField]
+    private AnimationClip _walkClip = null;
+    [SerializeField]
+    private AnimationClip _RunClip = null;
+    [SerializeField]
+    private AnimationClip _attack1Clip = null;
+    [SerializeField]
+    private AnimationClip _attack2Clip = null;
+    [SerializeField]
+    private AnimationClip _deathClip = null;
+
+    private Animation _animation = null;
+
+    private EnemyState _state = default;
+
+    void Start()
+    {
+        _state = GetComponent<EnemyBrain>()._state;
+        _animation = GetComponent<Animation>();
+
+        _animation[_idleClip.name].wrapMode = WrapMode.Loop;
+        _animation[_walkClip.name].wrapMode = WrapMode.Loop;
+        _animation[_RunClip.name].wrapMode = WrapMode.Loop;
+        _animation[_attack1Clip.name].wrapMode = WrapMode.Once;
+        _animation[_attack2Clip.name].wrapMode = WrapMode.Once;
+        _animation[_deathClip.name].wrapMode = WrapMode.Once;
+    }
+
+    void Update()
+    {
+        AnimationCtrl();
+    }
+
+    void AnimationCtrl()
+    {
+        switch (_state)
+        {
+            case EnemyState.Wait:
+            case EnemyState.Idle:
+                _animation.CrossFade(_idleClip.name);
+                break;
+            case EnemyState.Chase:
+                _animation.CrossFade(_RunClip.name);
+                break;
+            case EnemyState.Walk:
+                _animation.CrossFade(_walkClip.name);
+                break;
+            case EnemyState.Attack:
+                _animation.CrossFade(_attack1Clip.name);
+                break;
+            case EnemyState.Death:
+                _animation.CrossFade(_deathClip.name);
+                break;
+            default:
+                break;
+        }
+    }
+}
--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -4,51 +4,157 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+    private EnemyState _state = EnemyState.Idle;
+
     [SerializeField]
     private float _speed = 5.0f;
 
-    private Transform _target = null;
-    private Rigidbody _rigidbody = null;
-    private EnemyEyes _eyes = null;
+    private GameObject _target = null;
+    private Transform _targetTransform = null;
+    private Vector3 _targetPos = Vector3.zero;
 
-    public enum EnemyState
-    {
-        Idle,
-        Chase
-    }
-
-    public EnemyState state = EnemyState.Idle;
+    [Header("Fight properties")]
+    private float _hp = 500;
+    private float _attackRange = 1.5f;
+    private float _attackDamage = 10.0f;
 
     void Start()
     {
-        _target = FindObjectOfType<CharacterMove>().transform;
-        _rigidbody = GetComponent<Rigidbody>();
-        _eyes = GetComponent<EnemyEyes>();
+        _state = GetComponent<EnemyBrain>()._state;
     }
 
     void Update()
     {
-        switch (state)
+        CkState();
+    }
+
+    void CkState()
+    {
+        switch (_state)
         {
             case EnemyState.Idle:
+                SetIdle();
                 break;
             case EnemyState.Chase:
-                Move();
+            case EnemyState.Walk:
+                SetMove();
+                break;
+            case EnemyState.Attack:
+                SetAtk();
+                break;
+            default:
                 break;
         }
     }
 
-    void Move()
+    void SetMove()
     {
-        Vector3 move = (_target.position - transform.position).normalized;
-        _rigidbody.MovePosition(_rigidbody.position + move * _speed * Time.deltaTime);
+        Vector3 distance = Vector3.zero;
+        Vector3 posLookAt = Vector3.zero;
+
+        switch (_state)
+        {
+            case EnemyState.Walk:
+                if (_targetPos != Vector3.zero)
+                {
+                    distance = _targetPos - transform.position;
+
+                    if (distance.magnitude < _attackRange)
+                    {
+                        StartCoroutine(SetWait());
+                        return;
+                    }
+
+                    posLookAt = new Vector3(_targetPos.x, transform.position.y, _targetPos.z);
+                }
+                break;
+            case EnemyState.Chase:
+                if (_target != null)
+                {
+                    distance = _target.transform.position - transform.position;
+                    if (distance.magnitude < _attackRange)
+                    {
+                        _state = EnemyState.Attack;
+                        return;
+                    }
+                    posLookAt = new Vector3(_target.transform.position.x, transform.position.y, _target.transform.position.z);
+                }
+                break;
+            default:
+                break;
+        }
+
+        Vector3 direction = distance.normalized;
+
+        direction = new Vector3(direction.x, 0, direction.z);
+
+        Vector3 amount = direction * _speed * Time.deltaTime;
+
+        transform.Translate(amount, Space.World);
+
+        transform.LookAt(posLookAt);
+    }
+
+    IEnumerator SetWait()
+    {
+        _state = EnemyState.Wait;
+        float timeWait = Random.Range(1.0f, 3.0f);
+        yield return new WaitForSeconds(timeWait);
+        _state = EnemyState.Idle;
+    }
+
+    void OnCkTarget(GameObject target)
+    {
+        _target = target;
+        _targetTransform = _target.transform;
+        _state = EnemyState.Chase;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Bullet"))
         {
-            _rigidbody.AddForce(other.transform.position * -1, ForceMode.Impulse);
+            _hp -= 10;
+            if (_hp > 0)
+            {
+                //Instantiate(_effectDamage, other.transform.position, Quaternion.identity);
+
+                //effectDamageTween();
+            }
+            else
+            {
+                _state = EnemyState.Death;
+            }
+        }
+    }
+
+    void SetAtk()
+    {
+        float distance = Vector3.Distance(_targetTransform.position, transform.position);
+        if (distance > _attackRange + 0.5f)
+        {
+            _state = EnemyState.Chase;
+        }
+    }
+
+    void SetIdle()
+    {
+        if (_target == null)
+        {
+            _targetPos = new Vector3(transform.position.x + Random.Range(-10.0f, 10.0f), transform.position.y + 1000f, transform.position.z + Random.Range(-10.0f, 10.0f));
+            Ray ray = new Ray(_targetPos, Vector3.down);
+
+            RaycastHit infoRayCast = new RaycastHit();
+
+            if (Physics.Raycast(ray, out infoRayCast, Mathf.Infinity))
+            {
+                _targetPos.y = infoRayCast.point.y;
+            }
+            _state = EnemyState.Walk;
+        }
+        else
+        {
+            _state = EnemyState.Chase;
         }
     }
 }
